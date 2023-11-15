076fda387fe61bac3d8d6208bb94fe16621252f6
mayurmonu <143120384+mayurmonu@users.noreply.github.com>
2023-11-15 12:08:52 -0600

    Update resource.js

--- routes/resource.js
+++ routes/resource.js
@@ -18,3 +18,23 @@ router.get('/lions/:id', lion_controller.lion_detail);
 // GET request for list of all lion items.
 router.get('/lions', lion_controller.lion_list);
 module.exports = router;
+//Handle lion update form on PUT.
+exports.lion_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await lion.findById( req.params.id)
+// Do updates of properties
+if(req.body.lion_type)
+toUpdate.lion_type = req.body.lion_type;
+if(req.body.lion_breed) toUpdate.lion_breed = req.body.lion_breed;
+if(req.body.lion_price) toUpdate.lion_price = req.body.lion_price;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
+};
