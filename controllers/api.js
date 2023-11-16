e2bfdbf2f374fc593f1e3b2e1172e641adc8f38d
mayurmonu <143120384+mayurmonu@users.noreply.github.com>
2023-11-16 00:06:49 -0600

    Update api.js

--- controllers/api.js
+++ controllers/api.js
@@ -7,3 +7,16 @@ exports.api = function(req, res) {
     res.write(']')
     res.send();
    };
+   //Handle building the view for updating a costume.
+// query provides the id
+exports.lion_update_Page = async function(req, res) {
+console.log("update view for item "+req.query.id)
+try{
+let result = await lion.findById(req.query.id)
+res.render('lionupdate', { title: 'lion Update', toShow: result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
