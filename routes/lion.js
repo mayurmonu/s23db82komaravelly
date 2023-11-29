a32801335f1b87a6cd72b6ea019a262360149bd8
mayurmonu <143120384+mayurmonu@users.noreply.github.com>
2023-11-29 17:07:35 -0600

    updated with secure

--- routes/lion.js
+++ routes/lion.js
@@ -7,6 +7,16 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 const lion_controlers= require('../controllers/lion');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ res.redirect("/login");
+ }
 /* GET lions */
 router.get('/', lion_controlers.lion_view_all_Page );
 
@@ -17,7 +27,7 @@ router.get('/detail', lion_controlers.lion_view_one_Page);
 /* GET create lion page */
 router.get('/create', lion_controlers.lion_create_Page);
 /* GET create update page */
-router.get('/update', lion_controlers.lion_update_Page);
+router.get('/update', secured, lion_controlers.lion_update_Page);
 /* GET delete lion page */
 router.get('/delete', lion_controlers.lion_delete_Page);
 
