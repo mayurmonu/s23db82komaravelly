2d2c87999ce1207a2a7aaefad47ee0e3b772c0c5
mayurmonu <143120384+mayurmonu@users.noreply.github.com>
2023-11-29 17:14:20 -0600

    updated with max min

--- models/lion.js
+++ models/lion.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose")
 const lionSchema = mongoose.Schema({
     lion_color: String,
     lion_breed: String,
-    lion_price: Number
-})
+    lion_price: { type:Number,
+        min : 0,
+        max : 300000,
+}})
 module.exports = mongoose.model("lion",lionSchema)
